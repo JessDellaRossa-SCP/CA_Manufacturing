de50416afbe6db2944e7e0f09a3b587bff1cd33a
JessDellaRossa <97637448+JessDellaRossa@users.noreply.github.com>
2022-12-28 17:06:37 -0800

    playing around with Shiny

--- App-1/app.R
+++ App-1/app.R
@@ -1,27 +1,54 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#### Shiny App for Exploring Manufacturing Activity in California ####
 
+#load packages -----------
 library(shiny)
+library(shinythemes)
 
-# Define UI
+#Load files ---------
+
+
+
+
+
+# Create User Interface -------
 ui <- fluidPage(
-  titlePanel("My Star Wars App"),
-  sidebarLayout(
-    sidebarPanel(),
+  theme= shinytheme("lumen"),
+  #App Title
+  headerPanel(title= tags$a(href="https://dtsc.ca.gov/scp/", tags$img(src = "SCP_Logo.png", height= 98.5, width= 128, 
+                                                                      style="float:left; margin-left: 5px; margin-right: 5px; margin-top: 5px; margin-bottom: 5px"))),
+ 
+  #Interactive Map Panel -----
+  #tabPanel("California Manufacturing Activity Interactive Map"),
+    sidebarLayout(
+    sidebarPanel(
+      #Title of this section of the panel
+      titlePanel("Desired Significant Habitat Ranking"),
+      #Instructions for reader
+      helpText("Explore California's aquatic and terrestrial significant habitats through filters. See the 'Significant Habitats' tab for more information"),
+      fluidRow(column(5,
+                      #Select which rank(s) to plot
+      checkboxGroupInput("aquatic", 
+                         h5("Aquatic Significant Habitat Rank"), 
+                         choices = list("Rank 5" = 5,
+                                        "Rank 4" = 4,
+                                        "Rank 3" = 3,
+                                        "Rank 2" = 2,
+                                        "Rank 1" = 1,
+                                        "Unranked" = 0),
+                         selected = 5),
+      checkboxGroupInput("terrestrial", 
+                         h5("Terrestrial Significant Habitat Rank"), 
+                         choices = list("Rank 5" = 5,
+                                        "Rank 4" = 4,
+                                        "Rank 3" = 3,
+                                        "Rank 2" = 2,
+                                        "Rank 1" = 1,
+                                        "Unranked" = 0),
+                         selected =5),
+      ))),
     mainPanel(
-      h6("Episode IV", align = "center"),
-      h6("A NEW HOPE", align = "center"),
-      h5("It is a period of civil war.", align = "center"),
-      h4("Rebel spaceships, striking", align = "center"),
-      h3("from a hidden base, have won", align = "center"),
-      h2("their first victory against the", align = "center"),
-      h1("evil Galactic Empire.")
+      h2("California Manufacturing Activity Interactive Map", align = "center"),
+      h5("by: Jessica DellaRossa & Elena Galkina", align = "center")
     )
   )
 )
@@ -32,3 +59,7 @@ server <- function(input, output) {}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
+#  #Navigation Bar at the top:
+#navbarPage("SCP Manufacturing Activity Map", shinytheme("lumen"),
+           #tabPanel("Interactive Map", fluid = TRUE, icon = icon("compass")),
